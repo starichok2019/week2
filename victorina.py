ccd50e2caab82415cc6724f52d1a5abbcc0e1950
starichok2019 <56167701+starichok2019@users.noreply.github.com>
2019-10-23 12:55:43 +0300

    start

--- victorina.py
+++ victorina.py
@@ -2,34 +2,49 @@
 print("Добро пожаловать в викторину 'Угадай год рождения знаменитости'!")
 print ("Вам будет задано 5 вопросов, постарайтесь ответить правильно на все!")
 answer = None
-while answer!= "H":
+while answer!= "stop_while":
     answer = ""
     score = 0
-# 1870
-year = int(input(" Введите год рождения В.И.Ленина"))
-if year == 1870:
-    score +=1
-# 1929
-year = int(input(" Введите год рождения В.М.Шукшина"))
-if year == 1929:
-    score += 1
-# 1938
-year = int(input(" Введите год рождения В.С.Высоцкого"))
-if year == 1938:
-    score += 1
-# 1870
-year = int(input(" Введите год рождения А.И.Куприна"))
-if year == 1870:
-    score += 1
-# 1672
-year = int(input(" Введите год рождения Петра Великого"))
-if year == 1672:
-    score += 1
+    # 1870
+    year = int(input(" Введите год рождения В.И.Ленина"))
+    if year == 1870:
+        score +=1
+    # 1929
+    year = int(input(" Введите год рождения В.М.Шукшина"))
+    if year == 1929:
+        score += 1
+    # 1938
+    year = int(input(" Введите год рождения В.С.Высоцкого"))
+    if year == 1938:
+        score += 1
+    # 1870
+    year = int(input(" Введите год рождения А.И.Куприна"))
+    if year == 1870:
+        score += 1
+    # 1672
+    year = int(input(" Введите год рождения Петра Великого"))
+    if year == 1672:
+        score += 1
+    print("")
     print("Игра окончена! Число правильных ответов:", score)
     print("Число неправильных ответов:", 5 - score)
-percent = score * 20
+    percent = score * 20
     print("Процент правильных ответов:", percent)
     print("Процент неправильных ответов:", 100 - percent)
-while answer!= "д" and answer!= "н":
-    answer = input ( "Хотите повтоить игру? (д/н)")
+
+    print("")
+    answer2 = None
+    while answer2!= "stop_while2":
+        povtor = input ( "Хотите повтоить игру? (д/н) (y/n)")
+        if povtor =="н" or povtor =="H" or povtor =="n" or povtor =="N":
+            answer = "stop_while"
+            answer2 = "stop_while2"
+        elif povtor =="д" or povtor =="Д" or povtor=="y"  or povtor=="Y":
+            answer2 = "stop_while2"
+        else:
+            print("")
+            print ("Введите корректное значение ещё раз!")
     print("Спасибо за участие!")
+
+
+
